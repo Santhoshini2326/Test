ac95436e6a78962929b7e6b2f952f3b9bd8d824f
Santhoshini2326 <122714757+Santhoshini2326@users.noreply.github.com>
2025-03-31 20:20:01 +0530

    Update Billing.java

--- Billing.java
+++ Billing.java
@@ -11,23 +11,13 @@ public class Bill{
             System.out.println("Enter 1 for Customer Details"+ '\n' +"Enter 2 for Item Details"+'\n'+"Enter 3 for Invoice details"
             +'\n'+"Enter 4 for Display the Invoice"+'\n'+"Enter 5 for searching the cutomer");
             int number = sc.nextInt();
-          //  CustomerDetails newCustomer = getCustomerDetails(sc, idnum);
             switch (number) 
             {
                 case 1:
-                CustomerDetails newCustomer = getCustomerDetails(sc, idnum,customers);
              
-                if(newCustomer == null )
-                {
-                    System.out.println("Invalid Information");
-                    System.out.println("------------------------------------------------");
-                }
-                else
-                {
-                    customers.add(newCustomer);
+                    getCustomerDetails(sc, idnum,customers);
                     idnum++;
                     System.out.println("------------------------------------------------");
-                }
                     break;
                 
                 case 2:
@@ -55,18 +45,7 @@ public class Bill{
                 case 5:
                 System.out.println("Searching customer in a list");
                 String customer = sc.next();
-              /*   for(CustomerDetails i : customers)
-                {
-                    if(customers(i).equals(customer))
-                    {
-                        System.out.println("The customer is present:" + customer);
-                    }
-                    else
-                    {
-                        System.out.println("The customer is not there ");
-                    }
-                    break;
-                }*/
+              
             }
         }
     }
@@ -74,7 +53,7 @@ public class Bill{
 
 
 
-    public static CustomerDetails getCustomerDetails(Scanner sc,Integer idnum,ArrayList<CustomerDetails> customers)
+    public static void getCustomerDetails(Scanner sc,Integer idnum,ArrayList<CustomerDetails> customers)
     {
             System.out.println("The ID number is :");
             System.out.println(idnum);
@@ -82,30 +61,27 @@ public class Bill{
             String custname= sc.next();
             System.out.println("Enter the phone");
             Integer phone = sc.nextInt();
-            System.out.println("Enter the mailid");
-            String email = sc.next();
-            boolean isValidEmail;
-            isValidEmail = getEmailCheck(email);
-            for(CustomerDetails customer : customers)
-            {
-            if((customer.getEmail()).equals(email))
-            {
-                System.out.println("Type the valid Mail");
-                email = sc.next();
-                isValidEmail = getEmailCheck(email);
-            }
-            }
-
-            if (!isValidEmail) 
-            {
-                return null;
-            }
+            String email = getValidEmail(customers,sc);
     
             CustomerDetails customerDetails = new CustomerDetails(custname,phone,email,idnum);
-        return customerDetails;
+            customers.add(customerDetails);
      }
 
-
+     public static String getValidEmail(ArrayList<CustomerDetails> customers, Scanner sc) 
+     {
+        System.out.println("Enter your email: ");
+        String email = sc.next();
+    
+        for (CustomerDetails customer : customers) {
+            if (customer.getEmail().equals(email) || !getEmailCheck(email)) { 
+                System.out.println("Invalid Email. Enter a valid one:");
+                return getValidEmail(customers, sc); 
+            }
+        }
+    
+        return email; 
+    }
+    
      public static boolean getEmailCheck(String email)
      {
         if(email.contains("@") && email.endsWith(".com"))
@@ -115,4 +91,14 @@ public class Bill{
         return false;
      }
 
+     public static ItemDetails  getItemDetails(Scanner sc)
+     {
+            System.out.println("Enter the item:");
+            String item = sc.next();
+            System.out.println("Enter the quantity:");  
+            int quantity = sc.nextInt();
+        
+        ItemDetails itemDetails = new ItemDetails(item, quantity);
+        return itemDetails ;
+     }
 }
